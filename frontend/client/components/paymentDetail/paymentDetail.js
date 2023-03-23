0d0a2540a5869c8e437b0bb9f4e01ce6922002ae
Maros Bednar <bednarmaros341@gmail.com>
2023-03-23 10:02:38 +0100

    Payment detail

--- frontend/client/components/paymentDetail/paymentDetail.js
+++ frontend/client/components/paymentDetail/paymentDetail.js
@@ -4,3 +4,13 @@ onDeliveryOptionChanged = (event) => {
 	document.querySelectorAll('.delivery-checkbox').forEach((c) => (c.checked = false));
 	event.checked = true;
 };
+
+onPaymentOptionChanged = (event) => {
+	document.querySelectorAll('.payment-checkbox').forEach((c) => (c.checked = false));
+	event.checked = true;
+};
+
+onOrderNowClicked = (event) => {
+	document.querySelectorAll('.payment-checkbox').forEach((c) => (c.checked = false));
+	event.checked = true;
+};
