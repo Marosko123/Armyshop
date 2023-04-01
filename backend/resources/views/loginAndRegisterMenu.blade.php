1b6d433eae22c0c0a3d3d21e35de980055d9d537
Maros Bednar <bednarmaros341@gmail.com>
2023-04-01 19:36:17 +0200

    added common styles

--- backend/resources/views/loginAndRegisterMenu.blade.php
+++ backend/resources/views/loginAndRegisterMenu.blade.php
@@ -7,7 +7,7 @@
     <title> Register </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/loginAndRegisterMenu.css') }}">
-    <link rel="stylesheet" href="../../common/styles.css">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <script src="{{ asset('js/loginAndRegisterMenu.js') }}"></script>
 </head>
 <body class="text-center">
