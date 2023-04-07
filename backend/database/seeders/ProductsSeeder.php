9a4136697949570f6c80859d3a9d2738db09b8fa
Michal Darovec <michal.darovec@gmail.com>
2023-04-07 22:07:16 +0200

    getting all products functionality + seeder

--- backend/database/seeders/ProductsSeeder.php
+++ backend/database/seeders/ProductsSeeder.php
@@ -20,7 +20,7 @@ class ProductsSeeder extends Seeder
             "name" => "AK-47",
             "price" => 599.99,
             "description" => "The AK-47 is a selective-fire, gas-operated assault rifle developed in the Soviet Union by Mikhail Kalashnikov in the late 1940s. It is one of the most widely used assault rifles in the world, known for its reliability, simplicity, and low production cost. It fires 7.62x39mm cartridges and can be used in various modes of fire, including semi-automatic and automatic.",
-            "image_url" => "no image, sorry",
+            "image_url" => "ak47-1.jpg",
             "subcategory_id" => 2,
             "license_needed" => true
         ]);
@@ -30,7 +30,7 @@ class ProductsSeeder extends Seeder
             "name" => "Desert Eagle",
             "price" => 299.99,
             "description" => "The Desert Eagle is a large semi-automatic handgun designed by Magnum Research Inc. It was first introduced in 1983 and has since become a popular choice for sport shooting, hunting, and personal defense.",
-            "image_url" => "no image, sorry",
+            "image_url" => "ak47-1.jpg",
             "subcategory_id" => 1,
             "license_needed" => true
         ]);
@@ -40,7 +40,7 @@ class ProductsSeeder extends Seeder
             "name" => "MAG-7",
             "price" => 499.99,
             "description" => "The MAG-7 is a pump-action shotgun designed and manufactured by the German company, Heinrich Krieghoff GmbH. It was introduced in 1997 and is known for its compact size and lightweight design, making it a popular choice for close-quarters combat and urban operations. The MAG-7 has a magazine capacity of 5 rounds and fires 12-gauge shells.",
-            "image_url" => "no image, sorry",
+            "image_url" => "ak47-1.jpg",
             "subcategory_id" => 4,
             "license_needed" => true
         ]);
@@ -50,7 +50,127 @@ class ProductsSeeder extends Seeder
             "name" => "Glock-18",
             "price" => 99.99,
             "description" => "The Glock 18 is a selective-fire, full-automatic machine pistol designed and manufactured by the Austrian company, Glock. It was introduced in 1986 and is known for its high rate of fire and reliability. The pistol has a 17-round magazine capacity and fires 9x19mm Parabellum cartridges.",
-            "image_url" => "no image, sorry",
+            "image_url" => "ak47-1.jpg",
+            "subcategory_id" => 1,
+            "license_needed" => true
+        ]);
+
+        Product::create([
+            "id" => 5,
+            "name" => "Glock-18",
+            "price" => 99.99,
+            "description" => "The Glock 18 is a selective-fire, full-automatic machine pistol designed and manufactured by the Austrian company, Glock. It was introduced in 1986 and is known for its high rate of fire and reliability. The pistol has a 17-round magazine capacity and fires 9x19mm Parabellum cartridges.",
+            "image_url" => "ak47-1.jpg",
+            "subcategory_id" => 1,
+            "license_needed" => true
+        ]);
+
+        Product::create([
+            "id" => 6,
+            "name" => "Glock-18",
+            "price" => 99.99,
+            "description" => "The Glock 18 is a selective-fire, full-automatic machine pistol designed and manufactured by the Austrian company, Glock. It was introduced in 1986 and is known for its high rate of fire and reliability. The pistol has a 17-round magazine capacity and fires 9x19mm Parabellum cartridges.",
+            "image_url" => "ak47-1.jpg",
+            "subcategory_id" => 1,
+            "license_needed" => true
+        ]);
+
+        Product::create([
+            "id" => 7,
+            "name" => "Glock-18",
+            "price" => 99.99,
+            "description" => "The Glock 18 is a selective-fire, full-automatic machine pistol designed and manufactured by the Austrian company, Glock. It was introduced in 1986 and is known for its high rate of fire and reliability. The pistol has a 17-round magazine capacity and fires 9x19mm Parabellum cartridges.",
+            "image_url" => "ak47-1.jpg",
+            "subcategory_id" => 1,
+            "license_needed" => true
+        ]);
+
+        Product::create([
+            "id" => 8,
+            "name" => "Glock-18",
+            "price" => 99.99,
+            "description" => "The Glock 18 is a selective-fire, full-automatic machine pistol designed and manufactured by the Austrian company, Glock. It was introduced in 1986 and is known for its high rate of fire and reliability. The pistol has a 17-round magazine capacity and fires 9x19mm Parabellum cartridges.",
+            "image_url" => "ak47-1.jpg",
+            "subcategory_id" => 1,
+            "license_needed" => true
+        ]);
+
+        Product::create([
+            "id" => 9,
+            "name" => "Glock-18",
+            "price" => 99.99,
+            "description" => "The Glock 18 is a selective-fire, full-automatic machine pistol designed and manufactured by the Austrian company, Glock. It was introduced in 1986 and is known for its high rate of fire and reliability. The pistol has a 17-round magazine capacity and fires 9x19mm Parabellum cartridges.",
+            "image_url" => "ak47-1.jpg",
+            "subcategory_id" => 1,
+            "license_needed" => true
+        ]);
+
+        Product::create([
+            "id" => 10,
+            "name" => "Glock-18",
+            "price" => 99.99,
+            "description" => "The Glock 18 is a selective-fire, full-automatic machine pistol designed and manufactured by the Austrian company, Glock. It was introduced in 1986 and is known for its high rate of fire and reliability. The pistol has a 17-round magazine capacity and fires 9x19mm Parabellum cartridges.",
+            "image_url" => "ak47-1.jpg",
+            "subcategory_id" => 1,
+            "license_needed" => true
+        ]);
+
+        Product::create([
+            "id" => 11,
+            "name" => "Glock-18",
+            "price" => 99.99,
+            "description" => "The Glock 18 is a selective-fire, full-automatic machine pistol designed and manufactured by the Austrian company, Glock. It was introduced in 1986 and is known for its high rate of fire and reliability. The pistol has a 17-round magazine capacity and fires 9x19mm Parabellum cartridges.",
+            "image_url" => "ak47-1.jpg",
+            "subcategory_id" => 1,
+            "license_needed" => true
+        ]);
+
+        Product::create([
+            "id" => 12,
+            "name" => "Glock-18",
+            "price" => 99.99,
+            "description" => "The Glock 18 is a selective-fire, full-automatic machine pistol designed and manufactured by the Austrian company, Glock. It was introduced in 1986 and is known for its high rate of fire and reliability. The pistol has a 17-round magazine capacity and fires 9x19mm Parabellum cartridges.",
+            "image_url" => "ak47-1.jpg",
+            "subcategory_id" => 1,
+            "license_needed" => true
+        ]);
+
+        Product::create([
+            "id" => 13,
+            "name" => "Glock-18",
+            "price" => 99.99,
+            "description" => "The Glock 18 is a selective-fire, full-automatic machine pistol designed and manufactured by the Austrian company, Glock. It was introduced in 1986 and is known for its high rate of fire and reliability. The pistol has a 17-round magazine capacity and fires 9x19mm Parabellum cartridges.",
+            "image_url" => "ak47-1.jpg",
+            "subcategory_id" => 1,
+            "license_needed" => true
+        ]);
+
+        Product::create([
+            "id" => 14,
+            "name" => "Glock-18",
+            "price" => 99.99,
+            "description" => "The Glock 18 is a selective-fire, full-automatic machine pistol designed and manufactured by the Austrian company, Glock. It was introduced in 1986 and is known for its high rate of fire and reliability. The pistol has a 17-round magazine capacity and fires 9x19mm Parabellum cartridges.",
+            "image_url" => "ak47-1.jpg",
+            "subcategory_id" => 1,
+            "license_needed" => true
+        ]);
+
+        Product::create([
+            "id" => 15,
+            "name" => "Glock-18",
+            "price" => 99.99,
+            "description" => "The Glock 18 is a selective-fire, full-automatic machine pistol designed and manufactured by the Austrian company, Glock. It was introduced in 1986 and is known for its high rate of fire and reliability. The pistol has a 17-round magazine capacity and fires 9x19mm Parabellum cartridges.",
+            "image_url" => "ak47-1.jpg",
+            "subcategory_id" => 1,
+            "license_needed" => true
+        ]);
+
+        Product::create([
+            "id" => 16,
+            "name" => "Glock-18",
+            "price" => 99.99,
+            "description" => "The Glock 18 is a selective-fire, full-automatic machine pistol designed and manufactured by the Austrian company, Glock. It was introduced in 1986 and is known for its high rate of fire and reliability. The pistol has a 17-round magazine capacity and fires 9x19mm Parabellum cartridges.",
+            "image_url" => "ak47-1.jpg",
             "subcategory_id" => 1,
             "license_needed" => true
         ]);
--- backend/public/components/products/products.js
+++ backend/public/components/products/products.js
@@ -15,15 +15,37 @@ function addToCart() {
 
 }
 
-async function getAllProducts() {
-    productList = await getFromUrl('/products');
-    productsHTML = '';
-    productList.products.forEach(function(product) {
+async function getAllProducts(pageNumber = 1) {
+    // get all products from backend
+    const productResponse = await getFromUrl('/products');
+    const productList = productResponse.products;
+
+    // get relevant products for page
+    const productsPerPage = 15;
+    const startIndex = (pageNumber - 1) * productsPerPage;
+    const endIndex = pageNumber * productsPerPage;
+    const pageProductList = productList.slice(startIndex, endIndex);
+
+    const notLikedImg = 'http://127.0.0.1:8000/images/productDetailImages/heart6.png';
+    const likedImg = 'http://127.0.0.1:8000/images/productDetailImages/heart4.png';
+
+    // create html for each product
+    let productsHTML = '';
+    for (const product of pageProductList) {
+        // find out if product is liked
+        let liked = false;
+        const userId = 1; // local storage
+        const productId = product.id;
+        const response = await getFromUrl(`/liked_products/${userId}/${productId}`);
+        if (response.status === 200) {
+            liked = true;
+        }
+        const likedVersion = liked ? likedImg : notLikedImg;
         productsHTML += `
         <div class="card m-3">
             <!-- image source: unsplash.com -->
             <img class="card-img-top"
-            src="http://127.0.0.1:8000/images/${product.image_url}"
+            src="http://127.0.0.1:8000/images/productDetailImages/${product.image_url}"
             alt="Card image cap">
             <div class="card-body d-flex align-items-center justify-content-between mx-auto">
                 <div>
@@ -31,7 +53,7 @@ async function getAllProducts() {
                     <p class="card-text">${product.price} €</p>
                 </div>
                 <!-- image source: flaticon.com -->
-                <img src="http://127.0.0.1:8000/images/productDetailImages/heart6.png" alt="" width="14%" class="liked-photo" onclick="toggleIcon(this)">
+                <img src="${likedVersion}" alt="" width="14%" class="liked-photo" onclick="toggleIcon(this)">
             </div>
                 <div class="card-body d-flex align-items-center justify-content-between mx-auto">
                     <!-- image source: flaticon.com -->
@@ -39,16 +61,30 @@ async function getAllProducts() {
                     <button type="button" class="btn btn-success btn-buy">Buy Now</button>
                 </div>
         </div>`;
-    });
+    }
 
     const cardContainer = document.getElementById('cards');
     cardContainer.innerHTML = productsHTML;
 }
 
-getProducts()
+console.time("slowFunction");
+getAllProducts();
+console.timeEnd("slowFunction");
 
 
 
+// license toggle checkmark
+function toggleCheckmark(checkmark) {
+    if (checkmark.classList.contains("checkmark")) {
+        checkmark.classList.remove("checkmark");
+        // remove the checkmark image
+        checkmark.src = "";
+    } else {
+        checkmark.classList.add("checkmark");
+        // add the checkmark image
+        checkmark.src = "http://127.0.0.1:8000/images/productDetailImages/checkmark.png";
+    }
+}
 
 // slider range
 const range1 = document.getElementById('myRange1');
