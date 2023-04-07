f70c26928929fe01563324211bd302bc651d25cf
Michal Darovec <michal.darovec@gmail.com>
2023-04-07 11:54:44 +0200

    added js for product changing amount and like icon

--- backend/public/components/productDetail/productDetail.js
+++ backend/public/components/productDetail/productDetail.js
@@ -2,26 +2,29 @@ onAddToBasketClicked = (event) => {
     // TODO: create popup added to basket
 };
 
-onLikedClicked = (event) => {
-    // TODO: change to next heart
-};
-
-
-
-
-
 function toggleIcon(likedPhoto) {
-    if (likedPhoto.classList.contains("liked")) {
+    if (likedPhoto.classList.contains("liked-photo-new")) { // removes from liked images
         // Image source: flaticon.com
-        likedPhoto.src = "../../resources/images/productDetailImages/heart6.png";
-        likedPhoto.classList.remove("liked");
-
-    
-    } else {
+        likedPhoto.src = "http://127.0.0.1:8000/images/productDetailImages/heart6.png";
+        likedPhoto.classList.remove("liked-photo-new");
+    } else { // adds to liked images
         // Image source: flaticon.com
-        likedPhoto.src = "../../resources/images/productDetailImages/heart4.png";
-        likedPhoto.classList.add("liked");
-
-        // 
+        likedPhoto.src = "http://127.0.0.1:8000/images/productDetailImages/heart4.png";
+        likedPhoto.classList.add("liked-photo-new");
     }
 }
+
+const amountInput = document.getElementById('amount')
+amountInput.value = 2;
+
+plusBtn.addEventListener("click", () => {
+    if (amountInput.value < 10) {
+        amountInput.value++;
+    }
+});
+
+minusBtn.addEventListener("click", () => {
+    if (amountInput.value > 1) {
+        amountInput.value--;
+    }
+});
