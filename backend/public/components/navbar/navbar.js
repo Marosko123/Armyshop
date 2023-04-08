0334452844e59abe0481928e2101c67301e4dc27
Maros Bednar <bednarmaros341@gmail.com>
2023-04-09 00:16:06 +0200

    Navbar without AI (for now)

--- backend/public/components/navbar/navbar.js
+++ backend/public/components/navbar/navbar.js
@@ -1,1 +1,124 @@
-console.log('test');
+console.log("test");
+
+document.querySelector(".search-input").addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        document.querySelector("#search-results-wrapper").innerHTML = "";
+        return (event.target.value = "");
+    }
+});
+
+document.querySelector("#landing-page").addEventListener("click", (event) => {
+    document.querySelector("#search-results-wrapper").innerHTML = "";
+    return (document.querySelector(".search-input").value = "");
+});
+
+const onSearchInputChanged = (event) => {
+    console.log(event.value);
+
+    const searchResult = createListOfResults([
+        {
+            img: "https://im9.cz/sk/iR/importprodukt-orig/e97/e973cf874f7c5b77944518743de8b9ff--mmf350x350.jpg",
+            label: "Nozik",
+            category: "weapons/knives",
+            price: "10",
+        },
+        {
+            img: "https://shop.militaryrange.sk/Content/custom/img_products_small/ko-1823-1.jpg",
+            label: "Ak47",
+            category: "weapons/rifles",
+            price: "584",
+        },
+        {
+            img: "https://im9.cz/sk/iR/importprodukt-orig/e97/e973cf874f7c5b77944518743de8b9ff--mmf350x350.jpg",
+            label: "Nozik",
+            category: "weapons/knives",
+            price: "10",
+        },
+        {
+            img: "https://shop.militaryrange.sk/Content/custom/img_products_small/ko-1823-1.jpg",
+            label: "Ak47",
+            category: "weapons/rifles",
+            price: "584",
+        },
+        {
+            img: "https://im9.cz/sk/iR/importprodukt-orig/e97/e973cf874f7c5b77944518743de8b9ff--mmf350x350.jpg",
+            label: "Nozik",
+            category: "weapons/knives",
+            price: "10",
+        },
+        {
+            img: "https://shop.militaryrange.sk/Content/custom/img_products_small/ko-1823-1.jpg",
+            label: "Ak47",
+            category: "weapons/rifles",
+            price: "584",
+        },
+        {
+            img: "https://im9.cz/sk/iR/importprodukt-orig/e97/e973cf874f7c5b77944518743de8b9ff--mmf350x350.jpg",
+            label: "Nozik",
+            category: "weapons/knives",
+            price: "10",
+        },
+        {
+            img: "https://shop.militaryrange.sk/Content/custom/img_products_small/ko-1823-1.jpg",
+            label: "Ak47",
+            category: "weapons/rifles",
+            price: "584",
+        },
+    ]);
+
+    document.getElementById("search-results-wrapper").innerHTML = searchResult;
+};
+
+const onShoppingCartClicked = () => {
+    window.location.href = "/shoppingcart";
+};
+
+const onProfileClicked = () => {
+    window.location.href = "/profile";
+};
+
+const onCategoryClicked = (category) => {
+    window.location.href = "/products/" + category;
+};
+
+const onSubCategoryClicked = (category, subcategory) => {
+    window.location.href = `/products/${category}/${subcategory}`;
+};
+
+const onSearchResultSelected = (result) => {
+    const resultString =
+        result
+            .querySelector(".search-result-row-category")
+            .innerHTML.toLowerCase()
+            .replace(" ", "_") +
+        "/" +
+        result
+            .querySelector(".search-result-row-label")
+            .innerHTML.toLowerCase()
+            .replace(" ", "_");
+    window.location.href = resultString;
+};
+
+const createListOfResults = (results) => {
+    let resultElement = '<div id="search-results">';
+    let i = 0;
+
+    for (let result of results) {
+        resultElement += `
+            <div class="search-result-row ${
+                i % 2 ? "pair-row" : ""
+            }" onclick="onSearchResultSelected(this)">
+                <img class="search-result-row-img" src="${result.img}" alt="">
+                <span class="search-result-row-label">${result.label}</span>
+                <span class="search-result-row-category">${
+                    result.category
+                }</span>
+                <span class="search-result-row-price">${result.price} €</span>
+            </div>
+        `;
+        i++;
+    }
+    resultElement += "</div>";
+
+    return resultElement;
+};
--- backend/resources/views/landingPage.blade.php
+++ backend/resources/views/landingPage.blade.php
@@ -25,18 +25,21 @@
 							src="{{ asset('images/logo/logoTransparent.png') }}" 
 							alt="Armyshop">
 					</a>
-					<input class="form-control search-input" type="search" placeholder="Search" aria-label="Search">
+					<div class="search-input-wrapper">
+						<input class="form-control search-input" type="search" placeholder="Search" aria-label="Search" onchange="onSearchInputChanged(this)">
+						<div id="search-results-wrapper"></div>
+					</div>
 					<!-- image source: freeiconspng.com -->
 					<img class="search-img" 
 						src="https://www.freeiconspng.com/uploads/search-icon-png-5.png" 
 						alt="Responsive image">
-					<button class="button shopping-cart__button">
+					<button class="button shopping-cart__button" onclick="onShoppingCartClicked()">
 					<!-- image source: freeiconspng.com -->
 						<img class="navbar-img" 
 							src="https://www.freeiconspng.com/uploads/bag-icon-6.png" 
 							alt="Responsive image">
 					</button>
-					<button class="button profile__button">
+					<button class="button profile__button" onclick="onProfileClicked()">
 					<!-- image source: freeiconspng.com -->
 						<img class="navbar-img" 
 							src="https://www.freeiconspng.com/uploads/msn-people-person-profile-user-icon--icon-search-engine-16.png" 
@@ -49,135 +52,141 @@
 			<nav id="secondary-navbar" class="navbar navbar-expand navbar-light bg-light">
 				<div class="navbar-dropdown container-fluid">
 					<div class="dropdown">
-						<button
-						class="btn dropdown-toggle"
-						type="button"
-						id="dropdownMenuButton"
-						data-mdb-toggle="dropdown"
-						aria-expanded="false"
-						>
-						Weapons
-						</button>
+						<div onclick="onCategoryClicked('weapons')">
+							<button
+								class="btn dropdown-toggle"
+								type="button"
+								id="dropdownMenuButton"
+								data-mdb-toggle="dropdown"
+								aria-expanded="false">
+								Weapons
+							</button>
+						</div>
 						<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-						<li><a class="dropdown-item" href="#">Pistols</a></li>
-						<li><a class="dropdown-item" href="#">Rifles</a></li>
-						<li><a class="dropdown-item" href="#">Submachine guns</a></li>
-						<li><a class="dropdown-item" href="#">Heavy</a></li>
-						<li><a class="dropdown-item" href="#">Knives</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('weapons', 'pistols')">Pistols</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('weapons', 'rifles')">Rifles</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('weapons', 'submachine_guns')">Submachine guns</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('weapons', 'heavy')">Heavy</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('weapons', 'knives')">Knives</a></li>
 						</ul>
 					</div>
 				</div>
 				<div class="navbar-dropdown container-fluid">
 					<div class="dropdown">
-						<button
-						class="btn dropdown-toggle"
-						type="button"
-						id="dropdownMenuButton"
-						data-mdb-toggle="dropdown"
-						aria-expanded="false"
-						>
-						Transport
-						</button>
+						<div onclick="onCategoryClicked('transport')">
+							<button
+								class="btn dropdown-toggle"
+								type="button"
+								id="dropdownMenuButton"
+								data-mdb-toggle="dropdown"
+								aria-expanded="false">
+								Transport
+							</button>
+						</div>
 						<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-						<li><a class="dropdown-item" href="#">Cars</a></li>
-						<li><a class="dropdown-item" href="#">Motocycles</a></li>
-						<li><a class="dropdown-item" href="#">Panzers</a></li>
-						<li><a class="dropdown-item" href="#">Planes</a></li>
-						<li><a class="dropdown-item" href="#">Boats</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('transport', 'cars')">Cars</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('transport', 'motocycles')">Motocycles</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('transport', 'panzers')">Panzers</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('transport', 'planes')">Planes</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('transport', 'boats')">Boats</a></li>
 						</ul>
 					</div>
 				</div>
 				<div class="navbar-dropdown container-fluid">
 					<div class="dropdown">
-						<button
-						class="btn dropdown-toggle"
-						type="button"
-						id="dropdownMenuButton"
-						data-mdb-toggle="dropdown"
-						aria-expanded="false"
-						>
-						Clothing
-						</button>
+						<div onclick="onCategoryClicked('clothing')">
+							<button
+								class="btn dropdown-toggle"
+								type="button"
+								id="dropdownMenuButton"
+								data-mdb-toggle="dropdown"
+								aria-expanded="false">
+								Clothing
+							</button>
+						</div>
 						<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-						<li><a class="dropdown-item" href="#">Jackets</a></li>
-						<li><a class="dropdown-item" href="#">Trousers</a></li>
-						<li><a class="dropdown-item" href="#">Shoes</a></li>
-						<li><a class="dropdown-item" href="#">Pants</a></li>
-						<li><a class="dropdown-item" href="#">Socks</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('clothing','jackets')">Jackets</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('clothing','trousers')">Trousers</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('clothing','shoes')">Shoes</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('clothing','pants')">Pants</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('clothing','socks')">Socks</a></li>
 						</ul>
 					</div>
 				</div>
 				<div class="navbar-dropdown container-fluid">
 					<div class="dropdown">
-						<button
-						class="btn dropdown-toggle"
-						type="button"
-						id="dropdownMenuButton"
-						data-mdb-toggle="dropdown"
-						aria-expanded="false"
-						>
-						Explosives
-						</button>
+						<div onclick="onCategoryClicked('explosives')">
+							<button
+								class="btn dropdown-toggle"
+								type="button"
+								id="dropdownMenuButton"
+								data-mdb-toggle="dropdown"
+								aria-expanded="false">
+								Explosives
+							</button>
+						</div>
 						<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-						<li><a class="dropdown-item" href="#">C4s</a></li>
-						<li><a class="dropdown-item" href="#">Nukes</a></li>
-						<li><a class="dropdown-item" href="#">Grenades</a></li>
-						<li><a class="dropdown-item" href="#">TNTs</a></li>
-						<li><a class="dropdown-item" href="#">Bombs</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('explosives', 'c4s')">C4s</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('explosives', 'nukes')">Nukes</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('explosives', 'grenades')">Grenades</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('explosives', 'tnts')">TNTs</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('explosives', 'bombs')">Bombs</a></li>
 						</ul>
 					</div>
 				</div>
 				<div class="navbar-dropdown container-fluid">
 					<div class="dropdown">
-						<button
-						class="btn dropdown-toggle"
-						type="button"
-						id="dropdownMenuButton"
-						data-mdb-toggle="dropdown"
-						aria-expanded="false"
-						>
-						Equipment
-						</button>
+						<div onclick="onCategoryClicked('equipment')">
+							<button
+								class="btn dropdown-toggle"
+								type="button"
+								id="dropdownMenuButton"
+								data-mdb-toggle="dropdown"
+								aria-expanded="false">
+								Equipment
+							</button>
+						</div>
 						<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-						<li><a class="dropdown-item" href="#">Backpacks</a></li>
-						<li><a class="dropdown-item" href="#">Hunting</a></li>
-						<li><a class="dropdown-item" href="#">Camping</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('equipment', 'backpacks')">Backpacks</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('equipment', 'hunting')">Hunting</a></li>
+							<li><a class="dropdown-item" onclick="onSubCategoryClicked('equipment', 'camping')">Camping</a></li>
 						</ul>
 					</div>
 				</div>
 				<div class="navbar-dropdown container-fluid">
 					<div class="dropdown">
-						<button
-						class="btn dropdown-toggle"
-						type="button"
-						id="dropdownMenuButton"
-						data-mdb-toggle="dropdown"
-						aria-expanded="false"
-						>
-						Accessories
-						</button>
+						<div onclick="onCategoryClicked('accessories')">
+							<button
+								class="btn dropdown-toggle"
+								type="button"
+								id="dropdownMenuButton"
+								data-mdb-toggle="dropdown"
+								aria-expanded="false">
+								Accessories
+							</button>
+						</div>
 						<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-						<li><a class="dropdown-item" href="#">Glasses</a></li>
-						<li><a class="dropdown-item" href="#">Face pain</a></li>
-						<li><a class="dropdown-item" href="#">Camouflage</a></li>
+						<li><a class="dropdown-item" onclick="onSubCategoryClicked('accessories', 'glasses')">Glasses</a></li>
+						<li><a class="dropdown-item" onclick="onSubCategoryClicked('accessories', 'face_paint')">Face pain</a></li>
+						<li><a class="dropdown-item" onclick="onSubCategoryClicked('accessories', 'camouflage')">Camouflage</a></li>
 						</ul>
 					</div>
 				</div>
 			</nav>
 			</div>
 		</div>
-		</header>
+	</header>
 
-		<!-- landing page -->
-		<div id="landing-page">
+	<!-- landing page -->
+	<div id="landing-page">
 		<!-- categories -->
 		<div id="categories">
 			<div class="container">
 			<h1 class="main-heading mt-5 display-1">Welcome to our ArmyShop</h1>
 			<div class="categories mt-5">
 				<div class="row justify-content-center">
-					<div class="col-sm-4 col-8 mb-4">
-						<div class="category mx-auto">
+					<div class="col-sm-4 col-8 mb-4" >
+						<div class="category mx-auto" onclick="onCategoryClicked('weapons')">
 						<div class="category-icon mb-3 mt-3">
 							<!-- image source: flaticon.com -->
 							<img src="{{ asset('images/productDetailImages/gun-pistol-icon.png') }}" alt="hello">
@@ -186,7 +195,7 @@
 						</div>
 					</div>
 					<div class="col-sm-4 col-8 mb-4">
-						<div class="category mx-auto">
+						<div class="category mx-auto" onclick="onCategoryClicked('transport')">
 						<div class="category-icon mb-3 mt-4">
 							<!-- image source: flaticon.com -->
 							<img src="{{ asset('images/productDetailImages/tank.png') }}" alt="hello">
@@ -195,7 +204,7 @@
 						</div>
 					</div>
 					<div class="col-sm-4 col-8 mb-4">
-						<div class="category mx-auto">
+						<div class="category mx-auto" onclick="onCategoryClicked('clothing')">
 						<div class="category-icon mb-2 mt-4">
 							<!-- image source: flaticon.com -->
 							<img src="{{ asset('images/productDetailImages/tshirt.png') }}" alt="hello">
@@ -206,7 +215,7 @@
 				</div>
 				<div class="row justify-content-center">
 					<div class="col-sm-4 col-8 mb-4">
-						<div class="category mx-auto">
+						<div class="category mx-auto" onclick="onCategoryClicked('explosives')">
 						<div class="category-icon mt-1 mb-2">
 							<!-- image source: flaticon.com -->
 							<img src="{{ asset('images/productDetailImages/bomb.png') }}" alt="hello">
@@ -215,7 +224,7 @@
 						</div>
 					</div>
 					<div class="col-sm-4 col-8 mb-4">
-						<div class="category mx-auto">
+						<div class="category mx-auto" onclick="onCategoryClicked('equipment')">
 						<div class="category-icon">
 							<!-- image source: flaticon.com -->
 							<img src="{{ asset('images/productDetailImages/backpack.png') }}" alt="hello">
@@ -224,7 +233,7 @@
 						</div>
 					</div>
 					<div class="col-sm-4 col-8 mb-4">
-						<div class="category mx-auto">
+						<div class="category mx-auto" onclick="onCategoryClicked('accessories')">
 						<div class="category-icon mb-2 mt-1">
 							<!-- image source: flaticon.com -->
 							<img src="{{ asset('images/productDetailImages/dog-tag.png') }}" alt="hello">
@@ -295,44 +304,44 @@
 			</section>
 		</div>
 		</div>
-
-		<!-- contact us -->
-		<footer>
-			<div id="contact-us" class="text-center mt-5">
-			<div class="contact-us-wrapper">
-				<div class="contact-us-column">
-				<h1>Contact us</h1>
-				<p>Phone: +421 9xx xxx xxx</p>
-				<p>Email: info@armyshop.xd</p>
-				</div>
-				<div class="contact-us-column">
-				<a href="#" target="_blank">
-					<!-- custom image -->
-					<img class="contact-us-logo" 
-					src="{{ asset('images/logo/logoTransparent.png') }}" 
-					alt="Armyshop">
-				</a>
-				<div class="contact-us-social-media">
-					<div>
-					<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" class="social-media-link" target="_blank">
-						<!-- image source: freepik.com -->
-						<img src="https://cdn.aboutstatic.com/file/d473bb55bdf6bd6430eeff7ed35fa4ca.png?quality=75&amp;height=480&amp;width=360" alt="media" class="social-media-img">
-					</a>
-					<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" class="social-media-link" target="_blank">
-						<!-- image source: freepik.com -->
-						<img src="https://cdn.aboutstatic.com/file/6a522d45a858eea2ccf7e71c2aa419fb.png?quality=75&amp;height=480&amp;width=360" alt="media" class="social-media-img">
-					</a>
-					<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" class="social-media-link" target="_blank">
-						<!-- image source: freepik.com -->
-						<img src="https://cdn.aboutstatic.com/file/75356ec63103aca8bf43a3071ed2a242.png?quality=75&height=75&width=75" alt="media" class="social-media-img">
-					</a>
-					</div>
-				</div>
-				</div>
-			</div>
-			</div>
-		</footer>
 	</div>
+
+	<!-- contact us -->
+	<footer>
+		<div id="contact-us" class="text-center mt-5">
+		<div class="contact-us-wrapper">
+			<div class="contact-us-column">
+			<h1>Contact us</h1>
+			<p>Phone: +421 9xx xxx xxx</p>
+			<p>Email: info@armyshop.xd</p>
+			</div>
+			<div class="contact-us-column">
+			<a href="#" target="_blank">
+				<!-- custom image -->
+				<img class="contact-us-logo" 
+				src="{{ asset('images/logo/logoTransparent.png') }}" 
+				alt="Armyshop">
+			</a>
+			<div class="contact-us-social-media">
+				<div>
+				<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" class="social-media-link" target="_blank">
+					<!-- image source: freepik.com -->
+					<img src="https://cdn.aboutstatic.com/file/d473bb55bdf6bd6430eeff7ed35fa4ca.png?quality=75&amp;height=480&amp;width=360" alt="media" class="social-media-img">
+				</a>
+				<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" class="social-media-link" target="_blank">
+					<!-- image source: freepik.com -->
+					<img src="https://cdn.aboutstatic.com/file/6a522d45a858eea2ccf7e71c2aa419fb.png?quality=75&amp;height=480&amp;width=360" alt="media" class="social-media-img">
+				</a>
+				<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" class="social-media-link" target="_blank">
+					<!-- image source: freepik.com -->
+					<img src="https://cdn.aboutstatic.com/file/75356ec63103aca8bf43a3071ed2a242.png?quality=75&height=75&width=75" alt="media" class="social-media-img">
+				</a>
+				</div>
+			</div>
+			</div>
+		</div>
+		</div>
+	</footer>
 
 	<script src="{{ asset('components/landingPage/landingPage.js') }}"></script>
 	<script src="{{ asset('components/navbar/navbar.js') }}"></script>
