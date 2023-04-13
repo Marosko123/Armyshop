0d696d1e31a8bc8a6f593379c0f7b0c83f6db560
Tomas Andel <tomasandel123@gmail.com>
2023-04-13 23:43:38 +0200

    userProfile data save to localStorage

--- backend/public/components/userProfile/userProfile.js
+++ backend/public/components/userProfile/userProfile.js
@@ -3,7 +3,7 @@ const orderHistoryBtn = document.getElementById('order-history-btn');
 const historyContainer = document.getElementById('orderHistoryContainer');
 
 orderHistoryBtn.addEventListener('click', function () {
-  if (historyContainer.style.display === 'none' || historyContainer.style.display=="") {
+  if (historyContainer.style.display === 'none' || historyContainer.style.display == "") {
     historyContainer.style.display = 'block';
   } else {
     historyContainer.style.display = 'none';
@@ -15,7 +15,7 @@ const wishListBtn = document.getElementById('wish-list-btn');
 const wishListContainer = document.getElementById('wishListContainer');
 
 wishListBtn.addEventListener('click', function () {
-  if (wishListContainer.style.display === 'none' || wishListContainer.style.display=="") {
+  if (wishListContainer.style.display === 'none' || wishListContainer.style.display == "") {
     wishListContainer.style.display = 'block';
   } else {
     wishListContainer.style.display = 'none';
@@ -25,45 +25,79 @@ wishListBtn.addEventListener('click', function () {
 //log-out handling
 const logoutButton = document.getElementById('log-out-btn');
 
-logoutButton.addEventListener('click', function() {
+logoutButton.addEventListener('click', function () {
   localStorage.removeItem('armyshop_currently_signed_in_user');
   window.location.href = "/";
   console.log("User logged-out");
 });
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   //redirect if user is not logged in
   data = JSON.parse(localStorage.getItem("armyshop_currently_signed_in_user"));
-  if (data === null) 
+  if (data === null)
     window.location.href = '/login';
   else
     document.querySelector('body').removeAttribute('hidden');
 
   //load user data into fields
-  console.log(data);
-
-  if(data.name)
+  if (data.name)
     document.getElementById('name').setAttribute("value", data.name);
 
-  if(data.email)
+  if (data.email)
     document.getElementById('email').setAttribute("value", data.email);
 
-  if(data.phone)
+  if (data.phone)
     document.getElementById('email').setAttribute("value", data.phone);
 
   nameInput = document.getElementById('name');
-  if(data.first_name)
+  if (data.first_name)
     nameInput.setAttribute("value", data.first_name + " ");
 
-  if(data.last_name)
-    nameInput.setAttribute("value", (nameInput.getAttribute("value") == null ? "": nameInput.getAttribute("value")) + data.last_name);
+  if (data.last_name)
+    nameInput.setAttribute("value", (nameInput.value == null ? "" : nameInput.value) + data.last_name);
 
-  if(data.address){
+  if (data.address) {
     addressFields = data.address.split(",");
     document.getElementById('address').setAttribute("value", addressFields[0]);
     document.getElementById('zip-code').setAttribute("value", addressFields[1]);
     document.getElementById('city').setAttribute("value", addressFields[2]);
     document.getElementById('country').setAttribute("value", addressFields[3]);
   }
-    
+});
+
+
+//save changes
+const saveChangesButton = document.getElementById('save-changes-btn');
+
+saveChangesButton.addEventListener('click', async function () {
+  oldData = JSON.parse(localStorage.getItem("armyshop_currently_signed_in_user"));
+
+
+  //add validation
+  //
+
+
+  address = document.getElementById('address').value +","+
+            document.getElementById('zip-code').value +","+
+            document.getElementById('city').value +","+
+            document.getElementById('country').value;
+  
+  data = {
+    "email": document.getElementById('email').value,
+    "first_name": document.getElementById('name').value.split(" ")[0],
+    "last_name": document.getElementById('name').value.split(" ")[1],
+    "age": null,
+    "address": address,
+    "license_picture": null,
+  };
+
+  console.log(data);
+  localStorage.setItem("armyshop_currently_signed_in_user",JSON.stringify(data));
+
+  
+  const productResponse = await getFromUrl('/products');
+  const productList = productResponse.products;
+
+  console.log(productList);
+
 });
